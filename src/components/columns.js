24e3164393660ee7bcfc428f6450b33bef9fe52b
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2024-03-06 11:29:25 +0545

    table-1

---
+++ src/components/columns.js
@@ -0,0 +1,35 @@
+
+//defining the columns for data tables
+export const COLUMNS = [
+    {
+        Header: 'Country',
+        Footer: 'Country',
+        accessor: 'country'
+    },
+    {
+        Header: 'Code 2',
+        Footer: 'Code 2',
+        accessor: 'code_2'
+    },
+    {
+        Header: 'Code 3',
+        Footer: 'Code 3',
+        accessor: 'code_3'
+    },
+    {
+        Header: 'Numeric Code',
+        Footer: 'Numeric Code',
+        accessor: 'numeric code'
+    },
+    {
+        Header: 'Link',
+        Footer :'Link',
+        accessor: 'link'
+    },
+    {
+        Header: 'Independent',
+        Footer: 'Independent',
+        accessor: 'independent'
+    }
+   
+]
